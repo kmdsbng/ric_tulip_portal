b19b536266b7df68b2443b69e11e4dc43ef8b979
Yoshihiro Kameda <kameda.sbng@gmail.com>
2025-02-23 20:00:43 +0900

    vote-result 修正

--- app/vote-result/page.tsx
+++ app/vote-result/page.tsx
@@ -8,207 +8,70 @@ import { Progress } from "@/components/ui/progress";
 import { motion, AnimatePresence } from "framer-motion";
 import { Flower2, CheckCircle2, XCircle } from "lucide-react";
 import PortalHeader from "@/components/PortalHeader";
+import React from "react";
 
-const quizData = [
-  {
-    question: "チューリップの名前の由来はどれ？",
-    options: ["帽子", "ターバン", "風車", "王冠"],
-    correctAnswer: 1,
-  },
-  {
-    question: "オランダが世界で最も多くのチューリップを生産していますが、1年間に生産される球根の数はどのくらい？",
-    options: ["約1,000万個", "約1億個", "約10億個", "約100億個"],
-    correctAnswer: 2,
-  },
-  {
-    question: "チューリップが正式に国花とされている国はどれでしょう？",
-    options: ["フランス", "オランダ", "エジプト", "トルコ"],
-    correctAnswer: 3,
-  },
-];
-
-const tulipImages = [
-  "/q1.png?height=150&width=150",
-  "/q2.png?height=150&width=150",
-  "/q3.png?height=150&width=150",
-];
-
-class Tulip {
-  readonly key: string;
-  readonly name: string;
-
-  constructor(key: string, name: string) {
-    this.key = key;
-    this.name = name;
-  }
+interface VoteCounts {
+  [tulipName: string]: number;
 }
 
-const TULIPS: Tulip[] = [
-  new Tulip('barcelona', 'バルセロナ'),
-  new Tulip('fox_trot', 'フォックストロット'),
-  new Tulip('negurije', 'ネグリジェ'),
-  new Tulip('charming_beauty', 'チャーミングビューティー'),
-  new Tulip('symphony', 'シンフォニー'),
-  new Tulip('ballerina', 'バレリーナ'),
-  new Tulip('match', 'マッチ'),
-  new Tulip('g_prince', 'ゴールデンプリンスクラウス'),
-  new Tulip('momotaro', '桃太郎'),
-  new Tulip('prince', 'プリンスクラウス'),
-  new Tulip('dynasty', 'ダイナスティ'),
-  new Tulip('negrita', 'ネグリタ'),
-  new Tulip('gaburiera', 'ガブリエラ'),
-  new Tulip('clear_water', 'クリアウォーター'),
-  new Tulip('strong_gold', 'ストロングゴールド'),
-  new Tulip('sinaeda', 'シナエダアモール'),
-  new Tulip('sara', 'サラ'),
-  new Tulip('green', 'グリーンスピリット'),
-  new Tulip('kuminz', 'クミンズ'),
-  new Tulip('mistless', 'ミストレス'),
-  new Tulip('sandol', 'サンドール'),
-  new Tulip('sanne', 'サンネ'),
-  new Tulip('continental', 'コンチネンタル'),
-  new Tulip('yohan', 'ヨハンクライフ'),
-  new Tulip('mariage', 'マリアージュ'),
-  new Tulip('izumi', 'イズミ'),
-  new Tulip('house', 'ハウステンボス'),
-  new Tulip('acepink', 'エースピンク'),
-  new Tulip('redproud', 'レッドプラウド'),
-  new Tulip('royal', 'ロイヤルテン'),
-  new Tulip('oreruan', 'オレルアンズ'),
-  new Tulip('fostery', 'フォステリーキング'),
-  new Tulip('redimpression', 'レッドインプレッション'),
-  new Tulip('world', 'ワールドピース'),
-  new Tulip('raribera', 'ラリベラ'),
-  new Tulip('aquarel', 'アクアレル'),
-  new Tulip('happy_upstar', 'ハッピーアップスター'),
-  new Tulip('menfis', 'メンフィス')
-] as const;
-
-export default function VoteResult() {
-  const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [selectedAnswer, setSelectedAnswer] = useState<number | null>(null);
-  const [confirmedAnswer, setConfirmedAnswer] = useState<number | null>(null);
-  const [score, setScore] = useState(0);
-  const [quizEnded, setQuizEnded] = useState(false);
-  const [showResult, setShowResult] = useState(false);
-  const [progress, setProgress] = useState(0);
+export default function VoteResult(): JSX.Element {
+  const [voteCounts, setVoteCounts] = useState<VoteCounts>({});
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    setProgress((currentQuestion / quizData.length) * 100);
-  }, [currentQuestion]);
-
-  const handleAnswer = (answerIndex: number) => {
-    setSelectedAnswer(answerIndex);
-  };
-
-
-  // const TULIPS: Tulip[] = [
-  //   {'barcelona': 'バルセロナ'},
-  //   'fox_trot': 'フォックストロット',
-  //   'negurije': 'ネグリジェ',
-  //   'charming_beauty': 'チャーミングビューティー',
-  //   'symphony': 'シンフォニー',
-  //   'ballerina': 'バレリーナ',
-  //   'match': 'マッチ',
-  //   'g_prince': 'ゴールデンプリンスクラウス',
-  //   'momotaro': '桃太郎',
-  //   'prince': 'プリンスクラウス',
-  //   'dynasty': 'ダイナスティ',
-  //   'negrita': 'ネグリタ',
-  //   'gaburiera': 'ガブリエラ',
-  //   'clear_water': 'クリアウォーター',
-  //   'strong_gold': 'ストロングゴールド',
-  
-  //   'sinaeda': 'シナエダアモール',
-  //   'sara': 'サラ',
-  //   'green': 'グリーンスピリット',
-  //   'kuminz': 'クミンズ',
-  //   'mistless': 'ミストレス',
-  //   'sandol': 'サンドール',
-  //   'sanne': 'サンネ',
-  //   'continental': 'コンチネンタル',
-  //   'yohan': 'ヨハンクライフ',
-  //   'mariage': 'マリアージュ',
-  //   'izumi': 'イズミ',
-  //   'house': 'ハウステンボス',
-  //   'acepink': 'エースピンク',
-  //   'redproud': 'レッドプラウド',
-  //   'royal': 'ロイヤルテン',
-  //   'oreruan': 'オレルアンズ',
-  //   'fostery': 'フォステリーキング',
-  //   'redimpression': 'レッドインプレッション',
-  //   'world': 'ワールドピース',
-  //   'raribera': 'ラリベラ',
-  //   'aquarel': 'アクアレル',
-  //   'happy_upstar': 'ハッピーアップスター',
-  //   'menfis': 'メンフィス',
-  // ] as const;
-
-
-  const handleNext = () => {
-    if (selectedAnswer === null) return;
-
-    setConfirmedAnswer(selectedAnswer);
-    setShowResult(true);
-
-    if (selectedAnswer === quizData[currentQuestion].correctAnswer) {
-      setScore(score + 1);
-    }
-
-    setTimeout(() => {
-      if (currentQuestion < quizData.length - 1) {
-        setCurrentQuestion(currentQuestion + 1);
-        setSelectedAnswer(null);
-        setConfirmedAnswer(null);
-        setShowResult(false);
-      } else {
-        setQuizEnded(true);
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch('https://fnrbegfgqf.execute-api.ap-northeast-1.amazonaws.com/vote_count');
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data: VoteCounts = await response.json();
+        setVoteCounts(data);
+      } catch (e: any) {
+        if (e instanceof Error) {
+          setError(e.message);
+        } else {
+          setError(String(e));
+        }
+      } finally {
+        setIsLoading(false);
       }
-    }, 1000);
-  };
+    };
 
-  const resetQuiz = () => {
-    setCurrentQuestion(0);
-    setSelectedAnswer(null);
-    setConfirmedAnswer(null);
-    setScore(0);
-    setQuizEnded(false);
-    setShowResult(false);
-    setProgress(0);
-  };
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
 
   return (
     <div className="min-h-screen flex flex-col bg-cover bg-center">
-      <main className="flex-grow">
-
+      <main className="flex-grow flex flex-col items-center justify-center p-8">
         <PortalHeader />
-
-
-
-        <div className="flex-grow flex flex-col items-center justify-center p-8">
-
-          <h1 className="text-4xl font-bold mb-4">
-            チューリップ投票
-          </h1>
-
-          <div className="mb-8">
-            <p className="text-lg">
-              お気に入りのチューリップに投票しよう！
-            </p>
-          </div>
-
-          <div className="grid grid-cols-3 gap-4">
-            {TULIPS.map((tulip) => (
-              <div key={tulip.key} className="bg-white rounded-lg shadow-md p-4">
-                <a href={`/tulip_detail.html?tulip_code=${tulip.key}`} className="block text-center p-2 bg-pink-100 hover:bg-pink-200 rounded-md text-blue-500 hover:underline">{tulip.name}</a>
-              </div>
-            ))}
-          </div>
-
+        <h1 className="text-4xl font-bold text-white mb-4">
+          投票結果
+        </h1>
+        <div className="text-white mb-8">
+          <p className="text-lg">
+            現在の投票結果です！
+          </p>
         </div>
-
-
-
+        <div className="grid grid-cols-3 gap-4">
+          {Object.entries(voteCounts).map(([tulipName, count]) => (
+            <div key={tulipName} className="bg-white rounded-lg shadow-md p-4">
+              <div className="text-center">
+                <span className="font-bold">{tulipName}:</span> {count} 票
+              </div>
+            </div>
+          ))}
+        </div>
       </main>
     </div>
   );

b16f17af1e89cfe60805723b123a54fba4e9a865
Yoshihiro Kameda <kameda.sbng@gmail.com>
2025-03-22 18:50:15 +0900

    mikuji2追加

--- app/mikuji2/page.tsx
+++ app/mikuji2/page.tsx
@@ -10,6 +10,7 @@ import { Flower2, CheckCircle2, XCircle } from "lucide-react";
 import PortalHeader from "@/components/PortalHeader";
 import React from "react";
 import { getTulipName } from "@/domain/tulip";
+import { mikujiItems } from "../mikuji/page";
 
 interface VoteCountItem {
   tulip_code: string;
@@ -21,152 +22,6 @@ interface VoteCounts {
   items: VoteCountItem[];
 }
 
-export const mikujiItems = [
-  {
-    img: "a.jpg",
-    name: "2025吉",
-  },
-  {
-    img: "a2.jpg",
-    name: "満開吉",
-  },
-  {
-    img: "aqua.jpg",
-    name: "ハイパー吉",
-  },
-  {
-    img: "baggys.jpg",
-    name: "バギ吉",
-  },
-  {
-    img: "barerina.jpg",
-    name: "ラッキーバレリーナ",
-  },
-  {
-    img: "bijutukan.jpg",
-    name: "ファッショニスタ",
-  },
-  {
-    img: "boru.jpg",
-    name: "中吉",
-  },
-  {
-    img: "yusyo.jpg",
-    name: "優勝",
-  },
-  {
-    img: "inthadoor.jpg",
-    name: "ビール吉",
-  },
-  {
-    img: "charming_beauty100.jpg",
-    name: "チャーミング＆エレガント",
-  },
-  {
-    img: "combi.jpg",
-    name: "俺たちゃ最強のコンビだぜー！吉",
-  },
-  {
-    img: "combi2.jpg",
-    name: "無敵",
-  },
-  {
-    img: "e.jpg",
-    name: "E吉",
-  },
-  {
-    img: "yurizaki.jpg",
-    name: "春らんまん吉",
-  },
-  {
-    img: "hasu.jpg",
-    name: "運気⤴アゲ⤴アゲ吉",
-  },
-  {
-    img: "heart.jpg",
-    name: "いっぱいちゅき♡吉",
-  },
-  {
-    img: "world100.jpg",
-    name: "ラブアンドピース吉",
-  },
-  {
-    img: "hinode.jpg",
-    name: "初日の吉",
-  },
-  {
-    img: "itirin.jpg",
-    name: "この世に一つだけの吉",
-  },
-  {
-    img: "kuro.jpg",
-    name: "黒に染まれ吉",
-  },
-  {
-    img: "liner.jpg",
-    name: "ライトアップ六甲ライナー吉",
-  },
-  {
-    img: "magipan.jpg",
-    name: "マジック吉",
-  },
-  {
-    img: "prince100.jpg",
-    name: "プリンス吉",
-  },
-  {
-    img: "nanohana.jpg",
-    name: "ダブル吉",
-  },
-  {
-    img: "peach.jpg",
-    name: "ミルクピーチ吉",
-  },
-  {
-    img: "red.jpg",
-    name: "大吉",
-  },
-  {
-    img: "orange_vandyke100.jpg",
-    name: "なにもかもうまくいく吉",
-  },
-  {
-    img: "river2.jpg",
-    name: "ほのぼの吉",
-  },
-  {
-    img: "negrita100.jpg",
-    name: "ラッキーバイオレット吉",
-  },
-  {
-    img: "sakura.jpg",
-    name: "十分咲き吉",
-  },
-  {
-    img: "syogun.jpg",
-    name: "八代将軍大吉宗",
-  },
-  {
-    img: "negurije100.jpg",
-    name: "ちょっとだけ吉",
-  },
-  {
-    img: "fox.jpg",
-    name: "ギガ吉",
-  },
-  {
-    img: "tettyan.jpg",
-    name: "ほぼ大吉",
-  },
-  {
-    img: "yuri2.jpg",
-    name: "フラッシュ！！吉",
-  },
-  {
-    img: "usami100.jpg",
-    name: "宇佐吉",
-  },
-] as const;
 
 export default function VoteResult(): JSX.Element {
   const [voteCounts, setVoteCounts] = useState<VoteCounts>({ total: 0, items: [] });
@@ -230,7 +85,6 @@ export default function VoteResult(): JSX.Element {
         )}
       </AnimatePresence>
       <main className="flex-grow">
-        <PortalHeader />
         <div>
           {
             mikujiItems.map((src, index) => (
@@ -245,21 +99,16 @@ export default function VoteResult(): JSX.Element {
         </div>
 
         <div className="flex-grow flex flex-col items-center justify-center">
-          <h1 className="text-2xl font-bold mb-2">
+          <h1 className="text-4xl font-bold mb-6 text-black shadow-md">
             チューリップみくじ
           </h1>
-          <div className="mb-6">
-            <p className="text-lg">
-              スクショを撮ってください
-            </p>
-          </div>
           <div className="">
             <div className="text-center flex flex-col items-center">
               <img
                 src={`/mikuji/${mikujiItems[currentImageIndex].img}`}
                 alt="Rotating Tulip"
-                width={150}
-                height={150}
+                width={300}
+                height={300}
                 className="object-cover rounded-lg shadow-md"
               />
               <div className="pt-2 text-lg">
